2950e46d5001297f24a0fc0be076b89852a92299
SsAaFfIiKk <chf700@yandex.ru>
2022-03-06 18:47:51 +0300

    add frontend

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from "react";
+
+
+export default function App() {
+
+    const [predict, setPredict] = useState("predict")
+
+    useEffect(() => {
+        fetch("https://teachingquality.onti.actcognitive.org/get_predict")
+            .then(res => res.json())
+            .then(out => { setPredict(out); console.log(out) })
+    })
+
+    return (
+        <div>
+            <div>
+                <img src="https://teachingquality.onti.actcognitive.org/get_img" alt="Не удалось загрузить">
+                </img>
+            </div>
+            <div>
+                {predict}
+            </div>
+        </div>
+    )
+}
