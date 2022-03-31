20cb79da92e15fef4228f144b9f2196eb9e0dde1
SsAaFfIiKk <chf700@yandex.ru>
2022-03-31 14:45:50 +0000

    add new url

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
     const [predict, setPredict] = useState("predict")
 
     useEffect(() => {
-        fetch("https://teachingquality.onti.actcognitive.org/get_predict")
+        fetch("https://dev.exclusive.onti.actcognitive.org/study/get_predict")
             .then(res => res.json())
             .then(out => { setPredict(out); console.log(out) })
     }, [])
@@ -14,7 +14,7 @@ export default function App() {
     return (
         <div>
             <div>
-                <img src="https://teachingquality.onti.actcognitive.org/get_img" alt="Не удалось загрузить">
+                <img src="https://dev.exclusive.onti.actcognitive.org/study/get_img" alt="Не удалось загрузить">
                 </img>
             </div>
             <div>
