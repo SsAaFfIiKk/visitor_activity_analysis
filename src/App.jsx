88d6ef562fd3ffa5b9176013ccbaf91b452c80d0
SsAaFfIiKk <chf700@yandex.ru>
2022-04-01 11:36:47 +0300

    add upload img

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,34 @@ import React, {useEffect, useState} from "react";
 
 
 export default function App() {
-
+    const upload_url = "https://dev.exclusive.onti.actcognitive.org/study/upload"
+    const get_img = "https://dev.exclusive.onti.actcognitive.org/study/get_img"
+    const predict_url = "https://dev.exclusive.onti.actcognitive.org/study/get_predict"
+    
+    const [selectedImage, setSelectedImage] = useState(null);
     const [predict, setPredict] = useState("predict")
 
-    useEffect(() => {
+    const sendImg =() => {
+        const data = new FormData()
+        data.append("file", selectedImage)
+        fetch(upload_url, { method: "post", body: data })
+        
         fetch("https://dev.exclusive.onti.actcognitive.org/study/get_predict")
             .then(res => res.json())
             .then(out => { setPredict(out); console.log(out) })
-    }, [])
+    }
 
     return (
         <div>
             <div>
-                <img src="https://dev.exclusive.onti.actcognitive.org/study/get_img" alt="Не удалось загрузить">
-                </img>
+                <input
+                    type="file"
+                    name="myImage"
+                    onChange={(event) => {
+                        setSelectedImage(event.target.files[0]);
+                        sendImg()
+                    }}
+                />
             </div>
             <div>
                 {predict}
