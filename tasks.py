73a0a508df9f1f66cf5b6af7dde801716691c2ac
SsAaFfIiKk <chf700@yandex.ru>
2022-04-08 13:15:11 +0300

    fix start

--- tasks.py
+++ tasks.py
@@ -9,4 +9,4 @@ warnings.filterwarnings("ignore")
 def start(ctx):
     config = ctx.model
     img.read_config(config)
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/study")
